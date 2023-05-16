3c75b39f1de4f2145ebb25f65f0933ef5df613c6
andre <andreas.gustafsson@vanillaconsulting.se>
2023-05-16 22:39:29 +0200

    modified for new versions of library, added requirements.txt

--- graph.py
+++ graph.py
@@ -3,29 +3,30 @@ import plotly.graph_objects as go
 import itertools
 import sys
 
+# IMPORTANT NOTICE: This scrips was modified to run on a windows host. If you desire to run this on a mac or linux based system then you need to change the double backslashes in all file paths to a single forward slash.
 def plot(x, y, graphName):
     fig = go.Figure(data=go.Barpolar(
         r = x, 
         theta = y,
         marker_color=["#C55" for _ in y],
         width=[5 for x in range(len(x))]
-    ),
-    )
-    fig.update_layout(template=None, 
-            polar=dict(
-                radialaxis = dict(range=[0,8.3],showticklabels=False)
-                ),
-            angularaxis=dict(
-                range=[0,180],
-                showticklabels=False)
-            )
-    fig.update_layout(
-            showlegend = False,
-            polar = dict(sector = [0,180])
-            )
-   # fig.update_polars(dict(angularaxis=dict(rotation=90, direction='clockwise'), sector=[0, 360]))
-    fig.write_image("graphs/%s.pdf"%graphName)
-    fig.write_image("graphs/%s.jpeg"%graphName)
+    ))
+    # The disabled code underneath merely applies styling to the chart. It was disabled when plotly was upgraded with breaking changes. Feel free to play around with the new version.
+    #fig.update_layout(template=None, 
+    #        polar=dict(
+    #            radialaxis = dict(range=[0,8.3],showticklabels=False)
+    #           ),
+    #       angularaxis=dict(
+    #           range=[0,180],
+    #           showticklabels=False)
+    #         )
+    #  fig.update_layout(
+    #          showlegend = False,
+    #          polar = dict(sector = [0,180])
+    #          )
+    # fig.update_polars(dict(angularaxis=dict(rotation=90, direction='clockwise'), sector=[0, 360]))
+    fig.write_image("graphs\\%s.pdf"%graphName)
+    fig.write_image("graphs\\%s.jpeg"%graphName)
 
 def getData(path):
     f = open(path, mode='r')
@@ -41,5 +42,5 @@ def plotDataFile(path, graphName):
     plot([1 for x in range(len(fd))], fd, graphName)
 
 for fileName in sys.argv[1:]:
-    name = fileName.split("/")[1].split(".")[0]
+    name = fileName.split("\\")[1].split(".")[0]
     plotDataFile(fileName, name)
